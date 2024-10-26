685f09ccc6286eeb26364fda6a8ea798b92c8db5
ZAINU FIRDAUS IMANSYAH <zazain031@gmail.com>
2024-10-26 18:35:09 +0700

    fix:form

--- src/components/ServiceKami/FormKonsultasi.jsx
+++ src/components/ServiceKami/FormKonsultasi.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import image_1 from "../../assets/Servis Kami/whatsapp-button.png";
 
 const FormKonsultasi = () => {
     return (
-        <div className="mx-auto w-[640px] bg-[#cc0001] rounded-xl h-[652px] font-poppins">
-            <h1 className="ml-[14px] pt-4 pl-4 text-white font-extrabold text-3xl">Konsultasikan dengan kami</h1>
-            <p className="ml-[14px] pt-1  text-md font-light pl-4 text-white"> Tim kami siap untuk membantu perjalananmu!</p>
-            <form action="" method="POST" className="mt-[30px]  ">
+        <div className="mx-auto w-[640px] bg-[#cc0001] rounded-xl h-[652px] font-poppins ">
+            <h1 className="ml-[14px] pt-4 pl-4 text-white font-extrabold text-3xl">
+                Konsultasikan dengan kami
+            </h1>
+            <p className="ml-[14px] pt-1  text-md font-light pl-4 text-white">
+                {" "}
+                Tim kami siap untuk membantu perjalananmu!
+            </p>
+            <form action="" method="POST" className="mt-[30px] mb-[30px]  ">
                 <div className="flex gap-[20px] mx-[25px]">
                     <div className="w-full  sm:w-1/2 ">
                         <div className="mb-5">
@@ -78,21 +84,39 @@ const FormKonsultasi = () => {
                         </div>
                     </div>
                 </div>
-                <div className="mb-5 px-3 ml-[14px]">
+
+                <div className="mb-[26px] ">
                     <label
-                        htmlFor="guest"
-                        className="mb-3 block text-base text-white font-extralight"
+                        htmlFor="date"
+                        className="mb-3 block ml-[30px]  text-base  text-white font-extralight"
                     >
-                        How many guests are you bringing?
+                        service
                     </label>
-                    <input
-                        type="number"
-                        name="guest"
-                        id="guest"
-                        placeholder="5"
-                        min="0"
-                        className="w-full appearance-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-                    />
+                    <select className="select ml-[26px] w-[590px] border border-[#e0e0e0] bg-white h-[38px] ">
+                        <option disabled selected className="font-extrabold">
+                            PILIH SERVICE
+                        </option>
+                        <option>
+                            <h1 className="font-bold text-3xl">Private Trip</h1>
+                            <br />
+                            <p>Dengan transportasi umum</p>
+                        </option>
+                        <option>
+                            <h1 className="font-bold text-3xl">Private Trip</h1>
+                            <br />
+                            <p>Full dengan MOBIL PRIBADI</p>
+                        </option>
+                        <option>
+                            <h1 className="font-bold text-3xl">Jasa local guide</h1>
+                            <br />
+                            <p>Setiap kota beda local guide</p>
+                        </option>
+                        <option>
+                            <h1 className="font-bold text-3xl">Land arrangement</h1>
+                            <br />
+                            <p>Untuk grup besar minimal 15 orang</p>
+                        </option>
+                    </select>
                 </div>
 
                 <div className="mb-[26px] mx-3 ">
@@ -100,22 +124,23 @@ const FormKonsultasi = () => {
                         htmlFor="date"
                         className="mb-3 block ml-[14px]  text-base  text-white font-extralight"
                     >
-                        Date
+                        service
                     </label>
                     <textarea
                         name="date"
                         id="date"
                         placeholder="Enter date here"
-                        className="w-[600px] rounded-md ml-[14px]  border border-[#e0e0e0] bg-white py-3 px-3 h-[114px] text-base  text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                        className="w-[590px] rounded-md ml-[14px]  border border-[#e0e0e0] bg-white py-3 px-3 h-[114px] text-base  text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                     />
                 </div>
 
                 <div className="flex justify-center gap-[29px]">
-                    <button className="hover:shadow-form rounded-3xl bg-[#6A64F1] py-3text-center text-base font-semibold text-white outline-none px-[59px] ">
-                        Submit
+                    <button className=" rounded-3xl bg-[#38c738] py-2 text-center  text-sm font-semibold gap-2 hover:bg-green-600 text-white outline-none px-[59px] flex items-center ">
+                        <img src={image_1} alt="" className="w-[30px]" />
+                        <p className="">Chat Via WA</p>
                     </button>
-                    <button className="hover:shadow-form rounded-3xl bg-[#6A64F1] py-3  text-center text-base font-semibold text-white outline-none px-[59px]">
-                        Submit
+                    <button className=" rounded-3xl text-[#cc0001]  bg-white py-2  text-center  font-semibold text-sm  outline-none px-[59px]">
+                        Gratis Konsultasi
                     </button>
                 </div>
             </form>
